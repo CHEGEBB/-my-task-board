9b98e2cd53783e03d4b35cf752ead8e66fca9b3b
CHEGEBB <chegephil24@gmail.com>
2024-06-22 11:53:53 +0300

    added components

---
+++ app/components/Do.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Do() {
+  return (
+    <div className="do-main">
+        <h1>Task To Do</h1>
+    </div>
+  )
+}
---
+++ app/components/Wont.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Wont() {
+  return (
+    <div className="wont-main">
+        <h1>Task Won’t Do</h1>
+    </div>
+  )
+}
