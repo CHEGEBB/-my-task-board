370df43bf5decf176b3276a67b64bfd658ecef62
CHEGEBB <chegephil24@gmail.com>
2024-06-22 12:01:38 +0300

    added sass styles for components

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Do from "./components/Do";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <div className="flex flex-col p-4 text-left bg-gray-200 main-content">
+      <div className="flex flex-col p-4 text-left bg-gray-200 main-content ">
       <h1>My Task Board</h1>
       <h2><small>Tasks to keep organised</small></h2>
 
