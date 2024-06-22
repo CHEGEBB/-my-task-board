6d2d7a6916109db31cbc829e83266ad1334406f1
CHEGEBB <chegephil24@gmail.com>
2024-06-22 12:52:29 +0300

    styles for progress

--- app/components/Progress.tsx
+++ app/components/Progress.tsx
@@ -6,11 +6,13 @@ import Image from "next/image";
 export default function Progress() {
   return (
     <div className="progress-main">
+      <div className="prog">
       <div className="progress-icon-container">
         <h1>⏰</h1>
       </div>
       <div className="content-progress">
         <h1>Task in Progress</h1>
+      </div>
       </div>
       <div className="progress">
         <Image src={Alarmcon} alt="Alarm Icon" width={30} quality={100} />
