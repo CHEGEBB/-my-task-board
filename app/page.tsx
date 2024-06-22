c37cb947215804a405a2a088660b6ccc7e366302
CHEGEBB <chegephil24@gmail.com>
2024-06-22 11:47:49 +0300

    added ne tailwind styles

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <div className="main-content ">
+      <div className="flex flex-col p-4 text-left bg-gray-200 main-content">
       <h1>My Task Board</h1>
       <h2><small>Tasks to keep organised</small></h2>
       </div>
