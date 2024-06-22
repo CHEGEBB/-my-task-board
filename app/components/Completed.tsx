ec09f35e0e99f165088f194b322c332a3ddc4481
CHEGEBB <chegephil24@gmail.com>
2024-06-22 14:10:55 +0300

    using tailwind

--- app/components/Completed.tsx
+++ app/components/Completed.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import '../sass/Completed.scss'
+import Completedcon from '../images/Done_round.svg'
+import Image from 'next/image'
 
 export default function Completed() {
   return (
     <div className="completed-main">
+      <div className="comp">
+      <div className="p-3 ml-5 bg-white rounded-xl completed-icon-container">
+          <h1>🏋️‍♂️</h1>
+        </div>
+        <div className="completed-progress">
         <h1>Task Completed</h1>
+        </div>
+     
+        </div>
+        <div className="completed">
+          <Image src={Completedcon} alt="Completed Icon" width={30} quality={100} />
+        </div>
     </div>
   )
 }
