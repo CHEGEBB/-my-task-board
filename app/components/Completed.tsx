38dab11eb7ca792b9bc855e79ce606ea440595b7
CHEGEBB <chegephil24@gmail.com>
2024-06-22 11:52:06 +0300

    added components

---
+++ app/components/Completed.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Completed() {
+  return (
+    <div className="completed-main">
+        <h1>Task Completed</h1>
+    </div>
+  )
+}
---
+++ app/components/Progress.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Progress() {
+  return (
+    <div className="progress-main">
+        <h1>
+        Task in Progress
+        </h1>
+    </div>
+  )
+}
