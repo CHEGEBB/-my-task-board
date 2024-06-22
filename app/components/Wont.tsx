d04f801bb64eec23f9b72f4770fdce8b7493fc9d
CHEGEBB <chegephil24@gmail.com>
2024-06-22 14:42:12 +0300

    adding styles

--- app/components/Wont.tsx
+++ app/components/Wont.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import '../sass/Def.scss'
+import Wontcon from "../images/close_ring_duotone-1.svg"
+import Image from 'next/image'
 
 export default function Wont() {
   return (
     <div className="wont-main">
+      <div className="wont-cont">
+        <div className="p-3 ml-3 bg-white rounded-xl wont-icon-container">
+          <h1>☕</h1>
+        </div>
+        <div className="wont-progress">
         <h1>Task Wont Do</h1>
+        </div>
+      </div>
+      <div className="flex justify-end p-2 mr-3 rounded-xl wont">
+        <Image src={Wontcon} alt="Wont Icon" width={30} quality={100} />
+      </div>
+        
     </div>
   )
 }
