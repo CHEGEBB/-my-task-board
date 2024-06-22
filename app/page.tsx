3695fce80f18fdf17f77e79c20f653407e63d205
CHEGEBB <chegephil24@gmail.com>
2024-06-22 11:56:57 +0300

    added components in parent

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
 import Image from "next/image";
+import Progress from "./components/Progress";
+import Completed from "./components/Completed";
+import Wont from "./components/Wont";
+import Do from "./components/Do";
 
 export default function Home() {
   return (
@@ -6,6 +10,20 @@ export default function Home() {
       <div className="flex flex-col p-4 text-left bg-gray-200 main-content">
       <h1>My Task Board</h1>
       <h2><small>Tasks to keep organised</small></h2>
+
+      <div className="progress">
+        <Progress />
+      </div>
+      <div className="completed">
+        <Completed />
+      </div>
+
+      <div className="deffered">
+        <Wont />
+      </div>
+      <div className="do">
+        <Do />
+      </div>
       </div>
 
     </main>
