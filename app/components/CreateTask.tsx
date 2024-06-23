1935ff1844bc482948b4ebdf5e8ec423865a44b1
CHEGEBB <chegephil24@gmail.com>
2024-06-23 11:01:43 +0300

    added from to add tasks

---
+++ app/components/CreateTask.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function CreateTask() {
+  return (
+    <div className="create-task-main">
+      <form className="form">
+        <h2>Task details</h2>
+      </form>
+        
+    </div>
+  )
+}
