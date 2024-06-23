c90caf908b2d01847c10dce64590667e7b979386
CHEGEBB <chegephil24@gmail.com>
2024-06-23 11:50:03 +0300

    added conditional rendering of create task

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,12 +1,17 @@
+// components/AddTask.tsx
 import React from "react";
 import Addcon from "../images/Add_round_duotone.svg";
 import Image from "next/image";
 import "../sass/Add.scss";
 
-export default function AddTask() {
+type AddTaskProps = {
+  onClick: () => void; // Define onClick prop as a function that takes no arguments and returns void
+};
+
+const AddTask: React.FC<AddTaskProps> = ({ onClick }) => {
   return (
     <div className="add-main">
-      <div className="add-cont">
+      <div className="add-cont" onClick={onClick} style={{ cursor: "pointer" }}>
         <div className="p-3 ml-3 rounded-xl add-icon-container">
           <Image src={Addcon} alt="Add Icon" width={30} quality={100} />
         </div>
@@ -16,4 +21,6 @@ export default function AddTask() {
       </div>
     </div>
   );
-}
+};
+
+export default AddTask;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image";
 import Progress from "./components/Progress";
 import Completed from "./components/Completed";
@@ -7,64 +8,66 @@ import AddTask from "./components/AddTask";
 import Logo from "./images/logo.svg";
 import Edit from "./images/Edit_duotone.svg";
 import CreateTask from "./components/CreateTask";
-import {useState} from 'react'
+import { useState } from 'react';
 
 export default function Home() {
-  const [isVisible ,SetIsVisible]=useState(false)
+  const [isVisible, SetIsVisible] = useState(false);
+
+  const handleClick = () => {
+    SetIsVisible(!isVisible);
+  };
+
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <div className="container">
-      <div className=" main-content">
-        <div className="header">
-          <div className="logo">
-            <Image
-              src={Logo}
-              alt="logo"
-              width={50}
-              height={50}
-            />
+        <div className="main-content">
+          <div className="header">
+            <div className="logo">
+              <Image
+                src={Logo}
+                alt="logo"
+                width={50}
+                height={50}
+              />
+            </div>
+            <div className="content-app">
+              <h1>My Task Board</h1>
+              <h2><small>Tasks to keep organised</small></h2>
+            </div>
+            <div className="edit">
+              <Image
+                src={Edit}
+                alt="edit"
+                width={50}
+                height={50}
+              />
+            </div>
           </div>
-          <div className="content-app">
-          <h1>My Task Board</h1>
-          <h2><small>Tasks to keep organised</small></h2>
+
+          <div className="actions">
+            <div className="progress">
+              <Progress />
+            </div>
+            <div className="completed">
+              <Completed />
+            </div>
+            <div className="deferred">
+              <Wont />
+            </div>
+            <div className="do">
+              <Do />
+            </div>
+            <div className="add">
+              <AddTask onClick={handleClick} />
+            </div>
           </div>
-          <div className="edit">
-            <Image
-              src={Edit}
-              alt="edit"
-              width={50}
-              height={50}
-            />
-          </div>
-       
-      
         </div>
-     
-      <div className="actions">
-
-      <div className="progress">
-        <Progress />
+        {isVisible &&
+          <div className="create-task">
+            <CreateTask />
+          </div>
+        }
       </div>
-      <div className="completed">
-        <Completed />
-      </div>
-
-      <div className="deffered">
-        <Wont />
-      </div>
-      <div className="do">
-        <Do />
-      </div>
-      <div className="add">
-        <AddTask />
-      </div>
-      </div>
-      </div>
-      <div className="create-task">
-        <CreateTask />
-      </div>
-      </div>
-
     </main>
   );
 }
