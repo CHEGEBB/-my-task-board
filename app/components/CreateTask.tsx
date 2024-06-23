c54cd0992a1040ff5e41792a4f53a3934f929fad
CHEGEBB <chegephil24@gmail.com>
2024-06-23 12:39:41 +0300

    added icons

--- app/components/CreateTask.tsx
+++ app/components/CreateTask.tsx
@@ -14,6 +14,26 @@ export default function CreateTask() {
         <label htmlFor="description">Description</label>
         <textarea name="description" id="description" cols={30} rows={20}></textarea>
         </div>
+        <div className="icon">
+          <div className="p-3 ml-3 bg-white rounded-xl con1">
+            <h1>👨‍💻</h1>
+          </div>
+          <div className="p-3 ml-3 bg-white con2 rounded-xl">
+            <h1>💬</h1>
+            </div>
+            <div className="p-3 ml-3 bg-white con3 rounded-xl">
+            <h1>☕</h1>
+            </div>
+            <div className="p-3 ml-3 bg-white con4 rounded-xl">
+            <h1>🏋️‍♂️</h1>
+            </div>
+            <div className="p-3 ml-3 bg-white con5 rounded-xl">
+            <h1>📚</h1>
+            </div>
+            <div className="p-3 ml-3 bg-gray-400 con6 rounded-xl">
+            <h1>🧑‍🤝‍🧑</h1>
+            </div>
+        </div>
       </form>
         
     </div>
