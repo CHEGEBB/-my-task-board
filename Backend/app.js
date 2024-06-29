f15193da897293ecffda941cf29eaa0d86c65eac
CHEGEBB <chegephil24@gmail.com>
2024-06-29 14:05:30 +0300

    connected to mongo db

--- Backend/app.js
+++ Backend/app.js
@@ -2,15 +2,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const taskRouter = require('./routes/TaskRoute');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/taskDB';
+const MONGODB_URI = process.env.MONGODB_URI;
 
-// Connect to MongoDB
+// Connect to MongoDB with database creation option
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    dbName: 'taskDB', // Specify the database name here
+    autoCreate: true, // Automatically create the database if it doesn't exist
 })
 .then(() => console.log('MongoDB connected...'))
 .catch((err) => console.error('MongoDB connection error:', err));
